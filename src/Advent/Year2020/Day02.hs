095470e0da9b841e6b0effedf05fa968581b1f69
1computer1 <onecomputer00@gmail.com>
2020-12-21 04:07:42 -0500

    solve 2020 day02

--- src/Advent/Year2020/Day02.hs
+++ src/Advent/Year2020/Day02.hs
@@ -4,9 +4,27 @@ module Advent.Year2020.Day02
     ) where
 
 import Advent.Solution
+import Data.Char
 
 solutionA :: Solution
-solutionA = undefined
+solutionA = Solution $ \input ->
+    let ps = map parsePassword $ lines input
+    in length $ filter validPassword ps
+
+parsePassword :: String -> (Int, Int, Char, String)
+parsePassword xs =
+    let (a, '-':xs') = span isDigit xs
+        (b, ' ':x:':':' ':password) = span isDigit xs'
+    in (read a, read b, x, password)
+
+validPassword :: (Int, Int, Char, String) -> Bool
+validPassword (a, b, x, xs) = a <= k && k <= b
+    where k = length (filter (== x) xs)
 
 solutionB :: Solution
-solutionB = undefined
+solutionB = Solution $ \input ->
+    let ps = map parsePassword $ lines input
+    in length $ filter validPassword' ps
+
+validPassword' :: (Int, Int, Char, String) -> Bool
+validPassword' (a, b, x, xs) = (xs !! (a - 1) == x) /= (xs !! (b - 1) == x)
