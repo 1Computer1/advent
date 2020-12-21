1f507239e678d1cf646714187a8eb78e4faba7c9
1computer1 <onecomputer00@gmail.com>
2020-12-21 06:19:26 -0500

    solve 2020 day06

--- src/Advent/Year2020/Day06.hs
+++ src/Advent/Year2020/Day06.hs
@@ -4,9 +4,24 @@ module Advent.Year2020.Day06
     ) where
 
 import Advent.Solution
+import Data.List.Split
+import qualified Data.Map as M
 
 solutionA :: Solution
-solutionA = undefined
+solutionA = Solution $ \input ->
+    let xss = map (splitOn "\n") $ splitOn "\n\n" input
+    in sum $ map countGroupOccs xss
+
+countGroupOccs :: [String] -> Int
+countGroupOccs xss = M.size $ countOccs (concat xss)
+
+countOccs :: String -> M.Map Char Int
+countOccs xs = M.fromListWith (+) $ zip xs (repeat 1)
 
 solutionB :: Solution
-solutionB = undefined
+solutionB = Solution $ \input ->
+    let xss = map (splitOn "\n") $ splitOn "\n\n" input
+    in sum $ map countGroupEvery xss
+
+countGroupEvery :: [String] -> Int
+countGroupEvery xss = M.size . M.filter (== length xss) $ countOccs (concat xss)
