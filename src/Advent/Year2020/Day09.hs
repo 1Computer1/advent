9a4cd529cba2e6af7a7c07ba39164206667d1c44
1computer1 <onecomputer00@gmail.com>
2020-12-21 22:17:08 -0500

    solve 2020 day09

--- src/Advent/Year2020/Day09.hs
+++ src/Advent/Year2020/Day09.hs
@@ -1,12 +1,65 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Advent.Year2020.Day09
     ( solutionA
     , solutionB
     ) where
 
 import Advent.Solution
+import Data.List.Split
+import qualified Data.Sequence as S
+import Data.Monoid
 
 solutionA :: Solution
-solutionA = undefined
+solutionA = Solution $ \input ->
+    let ns = map read $ lines input
+        windows = divvy 26 1 ns
+        Just n = getFirst $ foldMap (First . invalidWindow) windows
+    in n
+
+invalidWindow :: [Int] -> Maybe Int
+invalidWindow xs =
+    if any (\(a, b) -> a /= b && a + b == n) ((,) <$> ns <*> ns)
+        then Nothing
+        else Just n
+    where Just (ns, n) = unsnoc xs
+
+unsnoc :: [a] -> Maybe ([a], a)
+unsnoc [] = Nothing
+unsnoc [x] = Just ([], x)
+unsnoc (x:xs) = Just (x:a, b)
+    where Just (a, b) = unsnoc xs
+
+data Slide = Slide
+    { tsum :: Int
+    , took :: S.Seq Int
+    , more :: [Int]
+    }
+
+makeSlide :: [Int] -> Slide
+makeSlide xs = Slide 0 S.empty xs
+
+takeLess :: Slide -> Slide
+takeLess s@(Slide { tsum, took, more }) = case took of
+    x S.:<| xs -> Slide (tsum - x) xs more
+    S.Empty -> s
+
+takeMore :: Slide -> Slide
+takeMore s@(Slide { tsum, took, more }) = case more of
+    (x:xs) -> Slide (tsum + x) (took S.|> x) xs
+    [] -> s
+
+sumToN :: Int -> Slide -> S.Seq Int
+sumToN target s =
+    case compare (tsum s) target of
+        EQ -> took s
+        LT -> sumToN target (takeMore s)
+        GT -> sumToN target (takeLess s)
 
 solutionB :: Solution
-solutionB = undefined
+solutionB = Solution $ \input ->
+    let ns = map read $ lines input
+        windows = divvy 26 1 ns
+        Just n = getFirst $ foldMap (First . invalidWindow) windows
+        ms = sumToN n (makeSlide ns)
+    in minimum ms + maximum ms
