75f73883d0cdb068aa8fb3ca733c9eb456c71f86
1computer1 <onecomputer00@gmail.com>
2020-12-26 05:41:21 -0500

    sovle 2020 day10

--- src/Advent/Year2020/Day10.hs
+++ src/Advent/Year2020/Day10.hs
@@ -4,9 +4,25 @@ module Advent.Year2020.Day10
     ) where
 
 import Advent.Solution
+import Data.List
 
 solutionA :: Solution
-solutionA = undefined
+solutionA = Solution $ \input ->
+    let ns = map read $ lines input :: [Int]
+        ms = 0 : sort ns <> [maximum ns + 3]
+        ds = zipWith (flip (-)) ms (tail ms)
+    in length (filter (== 1) ds) * length (filter (== 3) ds)
 
 solutionB :: Solution
-solutionB = undefined
+solutionB = Solution $ \input ->
+    let ns = map read $ lines input :: [Int]
+        ms = 0 : sort ns <> [maximum ns + 3]
+    in countPaths ms
+
+countPaths :: [Int] -> Int
+countPaths (x:xs) = go $ (x, 1) : zip xs (repeat 0)
+    where
+        go [(_, k)] = k
+        go ((n, k):ns) =
+            let (reachable, rest) = span (\(m, _) -> m - n <= 3) ns
+            in go $ map (\(m, h) -> (m, k + h)) reachable <> rest
